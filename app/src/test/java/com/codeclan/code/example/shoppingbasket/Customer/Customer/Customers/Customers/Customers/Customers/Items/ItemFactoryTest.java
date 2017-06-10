e6645884bc5fe2792c90aa165e7a6181bb8ea4eb
Daniel Garrido <daniel.garrido.info@gmail.com>
2017-06-10 14:13:40 +0100

    test id tracking in the Item Factory

--- app/src/test/java/com/codeclan/code/example/shoppingbasket/Customer/Customer/Customers/Customers/Customers/Customers/Items/ItemFactoryTest.java
+++ app/src/test/java/com/codeclan/code/example/shoppingbasket/Customer/Customer/Customers/Customers/Customers/Customers/Items/ItemFactoryTest.java
@@ -9,28 +9,31 @@ import static org.junit.Assert.*;
  * Created by Daniel Garrido on 10/06/2017.
  */
 public class ItemFactoryTest {
-    ItemFactory factory1;
     Item item1;
     Item item2;
     Item item3;
 
     @Before
     public void before(){
-        factory1 = new ItemFactory();
-        item1 = (Item) factory1.createItem();
-        item2 = (Item) factory1.createItem();
-        item3 = (Item) factory1.createItem();
+
     }
 
     @Test
     public void canKeepTrackOfIdMoreThanOneTime(){
+        ItemFactory factory2 = new ItemFactory();
+        item1 = (Item) factory2.createItem();
+        item2 = (Item) factory2.createItem();
+        item3 = (Item) factory2.createItem();
         assertEquals(1, item1.getId());
     }
 
     @Test
-    public void canKeepTrackOfID(){
-        assertEquals(6, item3.getId());
+    public void canKeepTrackOfFirstID(){
+        ItemFactory factory3 = new ItemFactory();
+        item1 = (Item) factory3.createItem();
+        assertEquals(1, item1.getId());
     }
+
 
 
 }
