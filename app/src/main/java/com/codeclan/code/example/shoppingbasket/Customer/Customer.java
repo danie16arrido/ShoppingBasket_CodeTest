bb719b4b4173a141222dccdb49941ae65b7319a2
Daniel Garrido <daniel.garrido.info@gmail.com>
2017-06-10 11:29:34 +0100

    add Customer package and class with a test file

---
+++ app/src/main/java/com/codeclan/code/example/shoppingbasket/Customer/Customer.java
@@ -0,0 +1,8 @@
+package com.codeclan.code.example.shoppingbasket.Customer;
+
+/**
+ * Created by Daniel Garrido on 10/06/2017.
+ */
+
+public class Customer {
+}
---
+++ app/src/test/java/com/codeclan/code/example/shoppingbasket/Customer/CustomerTest.java
@@ -0,0 +1,10 @@
+package com.codeclan.code.example.shoppingbasket.Customer;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Daniel Garrido on 10/06/2017.
+ */
+public class CustomerTest {
+
+}
