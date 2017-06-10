572e23fb18c639b57ef082dfee67c785164a4b7e
Daniel Garrido <daniel.garrido.info@gmail.com>
2017-06-10 12:50:14 +0100

    add class ShoppingBasket and test file

---
+++ app/src/test/java/com/codeclan/code/example/shoppingbasket/Customer/Customer/Customers/Customers/Customers/Customers/ShoppingBasquet/ShoppingBasketTest.java
@@ -0,0 +1,10 @@
+package com.codeclan.code.example.shoppingbasket.Customer.Customer.Customers.Customers.Customers.Customers.ShoppingBasquet;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Daniel Garrido on 10/06/2017.
+ */
+public class ShoppingBasketTest {
+
+}
